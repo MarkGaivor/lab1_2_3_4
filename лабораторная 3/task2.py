43a52d66fd769b229e2d5c63dfa6fe172f3aa6fb
MarkGaivor <153641030+MarkGaivor@users.noreply.github.com>
2024-01-27 13:37:34 +0300

    Update task2.py

--- лабораторная 3/task2.py
+++ лабораторная 3/task2.py
@@ -7,13 +7,14 @@ def socializator (group1,group2):
      len2=len(listik2)
      obshak1=[]
      for i in range(len1):
-        for j in range(len2):
-            if listik[i]==listik2[j]:
-               obshak1.append(listik[i])
+        if listik[i] not in obshak1:
+          for j in range(len2):
+              if listik[i]==listik2[j]:
+                obshak1.append(listik[i])
+                break
      if len(obshak1)==0:
          print('нету общих знакомых')
      else:
          print(obshak1)
 socializator(group1,group2)
-
 
